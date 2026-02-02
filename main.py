3b661ca0083de545f3f7d04c2f9f05a8bdce260a
Vlad Basalyha Ajax <vladbasalyhaajax@MacBook-Air-Vlad.local>
2026-02-02 21:44:42 +0200

    small changes

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Task(BaseModel):
+    task_name: str
+    priority: str
+    date: Union[str, None] = None
+
+
 @app.get("/")
 def read_root():
-
-    return {"Привіт": "Світ"}
-
+    return {"Завдання": "API для керування завданнями"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/tasks/{task_id}")
+def read_item(task_id: int, task: Union[str, None] = "Поки немає завдань"):
+    return {"task_id": task_id, "task": task}
+
+
+# @app.put("/tasks/{task_id}", response_model=Task)
+# def update_item(task_id: int, task: Task):
+#     return {"task_name": task.task, "task_id": task_id}
+    
